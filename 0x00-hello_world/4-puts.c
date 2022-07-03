83ada68bb735769d290a670f4ef7660069e0b447
cEbuka1 <kastecebuka@gmail.com>
2022-07-03 06:58:25 -0700

    Created script

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -Entry point of program
+ *
+ * puts- Prints the string to the screen
+ *
+ * Return - 0 if successful
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
