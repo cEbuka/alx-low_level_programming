04dc1dbf23609d731b62b32d66bbb56cf2e3e19e
cEbuka1 <kastecebuka@gmail.com>
2022-07-03 08:09:26 -0700

    Created script

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ int main(void)
 	long long int l;
 	float m;
 	printf("%s %lu %s\n", "Size of a char:", (unsigned long)sizeof(i), "byte(s)");
-	printf("%s %lu %s\n", "Size of a int:", (unsigned long)sizeof(j), "byte(s)");
+	printf("%s %lu %s\n", "Size of an int:", (unsigned long)sizeof(j), "byte(s)");
 	printf("%s %lu %s\n", "Size of a long int:", (unsigned long)sizeof(k), "byte(s)");
 	printf("%s %lu %s\n", "Size of a long long int:", (unsigned long)sizeof(l), "byte(s)");
 	printf("%s %lu %s\n", "Size of a float:", (unsigned long)sizeof(m), "byte(s)");
