4905f3d77c40263a9290692c4b6dfd3ee6922a7d
cEbuka1 <kastecebuka@gmail.com>
2022-07-04 16:51:23 -0700

    Created program

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 /**
-  * main -Entry point.
-  *
-  * @void: No params.
-  *
-  * Return: Always 0(success).
-  */
+ * main -Entry point.
+ *
+ * @void: No params.
+ *
+ * Return: Always 0(success).
+ */
 int main(void)
-{
+{		
 	char l;
-
 	for (l = 'a'; l <= 'z'; l++)
 		putchar(l);
 
 	putchar('\n');
-
 	return (0);
 }
