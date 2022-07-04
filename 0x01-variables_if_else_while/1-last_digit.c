c4404561613ebe68436ea0476789ebbb5c1c02a8
cEbuka1 <kastecebuka@gmail.com>
2022-07-04 15:36:41 -0700

    Created program

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,5 +25,9 @@ int main(void)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, nld);
 	}
 	else
+	{
 		printf("Last digit of %d is %d and is 0\n", n, nld);
+	}
+
 	return (0);
+}
