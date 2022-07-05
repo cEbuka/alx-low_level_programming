1a1f1311e83b8eaf62f66e18d5fa1ea0faa66c2b
cEbuka1 <kastecebuka@gmail.com>
2022-07-04 17:34:58 -0700

    Created program

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+  * main -Entry point.
+  *
+  * @void: No params.
+  *
+  * Return: Always 0(success).
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar((i % 10) + '0');
+		if (i == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
