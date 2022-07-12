f24e0cf3daf49adff74d86fc3983bfac5dda7d39
cEbuka1 <kastecebuka@gmail.com>
2022-07-12 16:22:58 -0700

    Created program

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *reset_to_98 - dereferences a pointer to 98
+ *
+ * @n : takes a pointer as param
+ *
+ * Return: 0(success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+	return (0);
+}
