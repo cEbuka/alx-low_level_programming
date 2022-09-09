e690950589e7f59b320bf1296d32fef29d7085ec
Ebuka_local <kastecebuka@gmail.com>
2022-09-09 20:34:04 +0000

    created the 4-puts.c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -Entry point of program
+ *
+ * puts: Prints the string to the screen
+ *
+ * @void: the program takes no paramters.
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
