e93c2213dc0fdb82293c910a23625e7f79c3a926
Ebuka_local <kastecebuka@gmail.com>
2022-09-13 20:23:45 +0000

    created 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit of a number
+ * @a: number
+ *
+ * Return: last digit of @a
+ */
+int print_last_digit(int a)
+{
+	int last;
+
+	if (a < 0)
+		a *= -1;
+	last = a % 10;
+	_putchar(last + '0');
+	return (last);
+}
