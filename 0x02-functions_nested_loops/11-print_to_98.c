074543d44942ceedbb937d764c036ec4b0d1e21f
Ebuka_local <kastecebuka@gmail.com>
2022-09-14 01:13:15 +0000

    fixed 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,10 @@ void print_to_98(int n)
 	{
 		for (; n >= 98; n--)
 		{
-			printf("%d, ", n);
+			if (n == 98)
+				printf("%d", n);
+			else
+				printf("%d, ", n);
 		}
 		printf("\n");
 	}
@@ -18,7 +21,10 @@ void print_to_98(int n)
 	{
 		for (; n <= 98; n++)
 		{
-			printf("%d, ", n);
+			if (n == 98)
+				printf("%d", n);
+			else
+				printf("%d, ", n);
 		}
 		printf("\n");
 	}
