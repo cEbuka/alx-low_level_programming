6cd2fdb2b73afd9e7594d50e7dd64c5d24ab56fb
Ebuka_local <kastecebuka@gmail.com>
2022-09-14 00:27:19 +0000

    created 10-add.c before task 9

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add -adds two integers
+ * @x: integer 1
+ * @y: integer 2;
+ *
+ * Return: result
+ */
+int add(int x, int y)
+{
+	int result;
+
+	result = x + y;
+	return (result);
+}
