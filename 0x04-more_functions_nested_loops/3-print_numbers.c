a936630956b32aa1c9ac9f687d446226b641ee4f
Ebuka_local <kastecebuka@gmail.com>
2022-09-16 01:26:59 +0000

    fix

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0 through 10
+ *
+ * Return: nothing
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
