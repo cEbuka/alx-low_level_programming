0105808f1ffc4c33d6dc2109e384624dada921ff
Ebuka_local <kastecebuka@gmail.com>
2022-09-16 01:35:02 +0000

    create 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0 through 10
+ *
+ * Return: nothing
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+}
