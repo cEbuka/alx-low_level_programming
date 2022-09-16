07c0aea9540b944faa575ec1f15c98cfa02007bb
Ebuka_local <kastecebuka@gmail.com>
2022-09-16 00:44:36 +0000

    created 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * isupper - checks for uppercase
+ * @c: character
+ *
+ * Return: 1 if true or 0 if not
+ */
+
+int _isupper(int c)
+{
+	int l;
+
+	l = 'A';
+	for (; l <='Z'; l++)
+		if (c == l)
+			return (1);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+# 0x04-more_functions_nested_loops
+
+** More C functions and nested loops **
