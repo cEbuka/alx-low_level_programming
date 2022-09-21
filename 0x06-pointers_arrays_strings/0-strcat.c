1a3100fd7c9d9bd03a3aa8eee026c10bf13547f6
Ebuka_local <kastecebuka@gmail.com>
2022-09-21 21:24:55 +0000

    fixed betty issues

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,12 +12,13 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-	for (i = 0; dest[i] != '\0'; i++);
+	for (i = 0; dest[i] != '\0'; i++)
+		;
 	/* i now pointing to null character in dest */
 	for (j = 0; src[j] != '\0'; j++)
-		dest[i+j] = src[j];
+		dest[i + j] = src[j];
 	/* add null terminate */
 	dest[i + j] = '\0';
-	
+
 	return (dest);
 }
