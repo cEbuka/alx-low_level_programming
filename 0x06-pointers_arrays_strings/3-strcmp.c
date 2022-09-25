732c898ffdd485bec967c4e3c3a61d6ebbc1707c
Ebuka_local <kastecebuka@gmail.com>
2022-09-25 15:14:06 +0000

    hot fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,13 +14,7 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		int s1_int = s1[i] + '0'; /* convert char to int */
-		int s2_int = s2[i] + '0'; /* convert char to int */
-
-		if (s1_int == s2_int)
-			return (0);
-		else
-			return (s1_int - s2_int);
+		return (s1[i] - s2[i]);
 	}
-	return (1);
+	return (0);
 }
