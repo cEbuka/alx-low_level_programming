42be9f9aab4c3e5664ce8ee05bf3d2103578e765
Ebuka_local <kastecebuka@gmail.com>
2022-10-04 08:04:26 +0000

    created 0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of @size and initialises it with @c
+ * @size: size of array
+ * @c: char in array
+ *
+ * Return: array containing @c
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *a;
+
+	if (size == 0)
+		return (NULL);
+	a = malloc(size * sizeof(c));
+	for(i = 0; i < size; i++)
+		*(a + i) = c;
+	return a;
+}
