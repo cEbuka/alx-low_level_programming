289da11af6ad9684d724a64c53e0d50795362cca
Ebuka_local <kastecebuka@gmail.com>
2022-10-11 23:20:28 +0000

    hot fix on 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,7 +13,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 
-	dog = malloc(sizeof(dog_t));
+	dog = malloc(sizeof(dog));
 	if (dog == NULL)
 		return (NULL);
 	dog->name = name;
