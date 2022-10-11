132aa76388341b24aa2e3bf732edd0a5806051a5
Ebuka_local <kastecebuka@gmail.com>
2022-10-11 20:00:49 +0000

    created 2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints struct dog
+ * @d: struct dog
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: %p\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+		else if (d->owner == NULL)
+			printf("Name: %s\nAge: %f\nOwner: %p\n", d->name, d->age, d->owner);
+		else
+			printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	}
+}
