67820e61bc837cfcf5099e245db91b5b4a8c11a2
Ebuka_local <kastecebuka@gmail.com>
2022-10-11 23:43:25 +0000

    hot fixing task 5

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,12 +12,37 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
+	int nlen, olen, i;
+
+
+	nlen = 0;
+	olen = 0;
+	
+	/*get length of name*/
+	while (name[nlen++])
+		;
+	
+	/*get length of owner*/
+	while (owner[olen++])
+		;
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	dog->name = name;
+
+	dog->name = malloc(nlen + 1);
+	if (d->name == NULL)
+		return (NULL);
+	for (i = 0; i < nlen; i++)
+		d->name[i] = name[i];
+
 	dog->age = age;
-	dog->owner = owner;
+
+	dog->owner = malloc(olen + 1);
+	if (d->owner == NULL)
+		return (NULL);
+	for (i = 0; i < olen; i++)
+		d->owner[i] = owner[i];
+
 	return (dog);
 }
