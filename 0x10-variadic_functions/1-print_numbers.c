4e71d50110973d3dc7e39c172878996628efac31
Ebuka_local <kastecebuka@gmail.com>
2022-10-13 23:47:33 +0000

    created 1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers with a separator
+ * @separator: separator
+ * @n: count of numbers
+ *
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list list;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(list, int));
+		if (i != (n - 1))
+			printf("%s", separator);
+		else
+			printf("\n");
+	}
+	va_end(list);
+}
