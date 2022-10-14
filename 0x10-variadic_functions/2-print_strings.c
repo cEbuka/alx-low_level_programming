bff774d0daac030548cdf45a1b45d53614fb6b20
Ebuka_local <kastecebuka@gmail.com>
2022-10-14 07:11:57 +0000

    fixed 1-print_numbers.c and created 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -2,21 +2,30 @@
 #include <stdio.h>
 
 /**
- * print_numbers - prints numbers with a separator
+ * print_strings - prints numbers with a separator
  * @separator: separator
  * @n: count of numbers
  *
  */
 
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
+	char *str;
 	unsigned int i;
 	va_list list;
 
 	va_start(list, n);
+	if (separator == NULL)
+
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(list, int));
+		str = va_arg(list, char *);
+
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
+
 		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
